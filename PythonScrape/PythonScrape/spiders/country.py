4d28b8f5d3ea7e0ed795e867db9228da0fe38468
Jovanzhuo <905335571@qq.com>
2017-05-15 22:31:40 +0800

    debug some mistake of country.py

--- PythonScrape/PythonScrape/spiders/country.py
+++ PythonScrape/PythonScrape/spiders/country.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 import scrapy
-from scrapy.linkextractors import LinkExtractor
-from scrapy.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors import LinkExtractor
+from scrapy.contrib.spiders import CrawlSpider, Rule
+
 from PythonScrape.items import PythonscrapeItem
+
 
 class CountrySpider(CrawlSpider):
     name = 'country'
@@ -10,20 +12,12 @@ class CountrySpider(CrawlSpider):
     start_urls = ['http://example.webscraping.com/']
 
     rules = (
-        #Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
-		Rule(LinkExtractor(allow='/index/', deny = '/user/'), follow = True),
-		Rule(LinkExtractor(allow='/view/', deny = '/user/'), callback='parse_item')
+        Rule(LinkExtractor(allow='/index/', deny='/user/'), follow=True),
+        Rule(LinkExtractor(allow='/view/', deny='/user/'), callback='parse_item')
     )
 
     def parse_item(self, response):
-        #i = {}
-        #i['domain_id'] = response.xpath('//input[@id="sid"]/@value').extract()
-        #i['name'] = response.xpath('//div[@id="name"]').extract()
-        #i['description'] = response.xpath('//div[@id="description"]').extract()
-        #return i
-		item = PythonscrapeItem()
-		name_css = 'tr#places_country_row td.w2p_fw::text'
-		item['name'] = response.css(name_css).extract()
-		pop_css = 'tr#places_population_row td.w2p_fw::text'
-		item['population'] = response.css(pop_css).extract()
-		return item
+        item = PythonscrapeItem()
+        item['name'] = response.css('tr#places_country__row td.w2p_fw::text').extract()
+        item['population'] = response.css('tr#places_population__row td.w2p_fw::text').extract()
+        return item
